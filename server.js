91ba6b9b507d21fddac97871f2d473127f28a4a0
Ola Assem <o.assem@gmail.com>
2018-03-03 20:30:13 +0200

    added GET Recipes

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const morgan = require('morgan');
 // parse JSON data sent in requests to this app
 const bodyParser = require('body-parser');
 
-// we import the ShoppingList model, which we'll
+// we import the ShoppingList and Recipes model, which we'll
 // interact with in our GET endpoint
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
+
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -16,7 +17,7 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
+// we're going to add some items to ShoppingList and Recipes
 // so there's some data to look at. Note that 
 // normally you wouldn't do this. Usually your
 // server will simply expose the state of the
@@ -25,11 +26,30 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+
+Recipes.create(
+	'protien shake', 
+	['coconut almond milk', 'choc whey protien powder', 'banana', 'peanutbutter powder', 'stevia']);
+Recipes.create(
+	'delish salmon', 
+	['salmon', 'honey', 'soy sauce', 'green onion', 'minced garlic']);
+Recipes.create(
+	'salad', 
+	['tomatoes', 'green onion', 'cucumber', 'parsley', 'salt', 'lemon']);
+
+
 // when the root of this route is called with GET, return
 // all current ShoppingList items by calling `ShoppingList.get()`
+// all current Recipes items by calling `Recipes.get()`
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+})
+
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
